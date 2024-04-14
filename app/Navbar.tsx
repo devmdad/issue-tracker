516ce4984c77aa04ce12cbcba4654bc11f76fffa
abc <abc@example.com>
2024-04-14 13:20:49 +0500

    Navbar Design and Prisma Setup

---
+++ app/Navbar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+
+const Navbar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex p-5 gap-x-5 border-b items-center">
+      <Link href="/">
+        <AiFillBug size={30} />
+      </Link>
+      <ul className="flex gap-x-5">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
