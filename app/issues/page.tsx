8128a36d38e0cf532d48ac50e7d61fbd34b0424e
abc <abc@example.com>
2024-04-15 20:11:30 +0500

    Refactor: Organize imports

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,7 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import Link from "../components/Link";
 import React from "react";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import { IssueStatusBadge, Link } from "../components";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
