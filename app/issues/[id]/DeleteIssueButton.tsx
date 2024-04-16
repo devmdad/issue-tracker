7cf1ee3119797c6ad51b5d3a7cbec28ac14f3686
abc <abc@example.com>
2024-04-16 20:01:05 +0500

    Adding delete issue confirmation

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,35 @@
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red" className="w-full">
-      <TrashIcon /> Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" className="w-full">
+          <TrashIcon /> Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Confirmation</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this item? Once done it can't be
+          undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gap="3">
+          <AlertDialog.Action>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Action>
+
+          <AlertDialog.Action>
+            <Button color="red" variant="soft">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
