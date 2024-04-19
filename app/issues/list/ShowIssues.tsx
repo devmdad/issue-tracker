e407faceceadc9848e3d4d2bffb1b8877406a7b0
abc <abc@example.com>
2024-04-19 13:38:52 +0500

    Paginate Issues

--- app/issues/list/ShowIssues.tsx
+++ app/issues/list/ShowIssues.tsx
@@ -1,4 +1,4 @@
-import { Table } from "@radix-ui/themes";
+import { Box, Table } from "@radix-ui/themes";
 import Link from "@/app/components/Link";
 import NextLink from "next/link";
 import React, { useState } from "react";
@@ -6,9 +6,10 @@ import { IssueStatusBadge } from "../../components";
 import { Issue, Status } from "@prisma/client";
 import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 import prisma from "@/prisma/client";
+import Pagination from "@/app/components/Pagination";
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue };
+  searchParams: { status: Status; orderBy: keyof Issue; page: string };
 }
 
 const ShowIssues = async ({ searchParams }: Props) => {
@@ -27,51 +28,67 @@ const ShowIssues = async ({ searchParams }: Props) => {
     ? { [searchParams.orderBy]: "asc" }
     : undefined;
 
+  const page = parseInt(searchParams.page) || 1;
+  const pageSize = 10;
+
   const issues = await prisma.issue.findMany({
     where: { status },
     orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize,
   });
 
-  return (
-    <Table.Root variant="surface" className="mt-5">
-      <Table.Header>
-        <Table.Row>
-          {columns.map((column) => (
-            <Table.ColumnHeaderCell key={column.value} className={column.classname}>
-              <NextLink
-                href={{
-                  query: { ...searchParams, orderBy: column.value },
-                }}
-              >
-                {column.label}
-                {column.value === searchParams.orderBy && (
-                  <ArrowUpIcon className="inline" />
-                )}
-              </NextLink>
-            </Table.ColumnHeaderCell>
-          ))}
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {issues.map((issue) => (
-          <Table.Row key={issue.id}>
-            <Table.Cell>
-              <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+  const issuesCount = await prisma.issue.count({ where: { status } });
 
-              <div className="block md:hidden">
-                <IssueStatusBadge status={issue.status} />
-              </div>
-            </Table.Cell>
-            <Table.Cell className="hidden md:table-cell">
-              <IssueStatusBadge status={issue.status} />
-            </Table.Cell>
-            <Table.Cell className="hidden md:table-cell">
-              {issue.createdAt.toDateString()}
-            </Table.Cell>
+  return (
+    <Box>
+      <Table.Root variant="surface" className="mt-5">
+        <Table.Header>
+          <Table.Row>
+            {columns.map((column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.classname}
+              >
+                <NextLink
+                  href={{
+                    query: { ...searchParams, orderBy: column.value },
+                  }}
+                >
+                  {column.label}
+                  {column.value === searchParams.orderBy && (
+                    <ArrowUpIcon className="inline" />
+                  )}
+                </NextLink>
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <div className="block md:hidden">
+                  <IssueStatusBadge status={issue.status} />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+      <Pagination
+        pageSize={pageSize}
+        currentPage={page}
+        itemsCount={issuesCount}
+      />
+    </Box>
   );
 };
 
